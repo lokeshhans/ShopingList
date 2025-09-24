ec0f3d64213d6b0383f04b688fa3c416fb772c6c
Lokesh Hans <Lokeshbaghelhans@gmail.com>
2025-09-24 22:37:48 +0530

    Adding the functionality of form validation and error handling

--- src/Components/InputItem/InputItem.jsx
+++ src/Components/InputItem/InputItem.jsx
@@ -1,27 +1,49 @@
-import React, { useState } from 'react'
 import './Inputitem.css'
 import 'react-toastify/dist/ReactToastify.css'
-import { ShowSucess } from '../../Utils/ShowTosts'
+import { ShowError, ShowSucess } from '../../Utils/ShowTosts'
+import { useForm } from 'react-hook-form'
+// import { useEffect } from 'react'
 
 
 function InputItem({addItem}) {
-  const [ inputVal, setInputVal] = useState('')
+  const {register, handleSubmit,formState: { errors }, reset} = useForm({
+    mode:'onChange'
+  })
+
+
+  // Errors .............................................
+  // useEffect(() => {
+  //   if(errors.item && errors.item.type == 'required' ){
+  //     ShowError("Item can't Empty");
+  //   }
+  //   else if(errors.item && errors.item.type == 'minLength'){
+  //     ShowError("Item name not less than 3")
+  //   }
+  // }, [errors.item && errors.item.type])
+  
+  const handleSubmission = (data) =>{
+    // console.log(data)
+    ShowSucess(`${data.item} Added SucessFully!`)
+    addItem(data.item)
+    reset();
+    
+  }
+  // console.log({...register("item", { required: true, minLength: 3 }) })
   return (
     <div className='Item-input-wrapper'>
-        <input type="text" 
-        placeholder='Add an item' 
-        value={inputVal} 
-        onChange={(e) => setInputVal(e.target.value)}/>
-        <button 
-          className='item-add-btn' 
-          onClick={()=>{
-            ShowSucess(`${inputVal} Added SucessFully!`)
-            addItem(inputVal)
-            setInputVal("")
-          }}
-          >
-            Add
-        </button>
+      <form onSubmit={handleSubmit(handleSubmission)} >
+          <input type="text" 
+          placeholder='Add an item' 
+          name='item'
+          {...register("item", { required: true, minLength: 3 }) }/>
+          <div>
+            {errors.item && errors.item.type == 'required' && <p>Item is missing</p> }
+            {errors.item && errors.item.type == 'minLength' && <p>Item name can't be less than 3 </p> }
+          </div>
+          <button className='item-add-btn' >
+              Add
+          </button>
+        </form>
     </div>
   )
 }
